150e1ee2adcfd6b04d87fcda5a836503f167695b
Nakshatra Vyas <nakshatravyas2004@gmail.com>
2024-03-27 15:34:41 +0530

    Implemented apis

--- backend/application/controllers.py
+++ backend/application/controllers.py
@@ -20,7 +20,11 @@ ALLOWED_EXTENSIONS = {'mp4', 'avi', 'mov'}
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-@app.route('/upload', methods=['POST'])
+@app.route("/")
+def index():
+    return jsonify(message="ha done bhai")
+
+@app.route("/upload", methods=['GET','POST'])
 def upload_file():
     if 'videoFile' not in request.files:
         return jsonify({'error': 'No file part'})
@@ -32,14 +36,16 @@ def upload_file():
     
     if video_File and allowed_file(video_File.filename):
         # filename = secure_filename(file.filename)
-        file_Path = os.path.join(app.config['UPLOAD_FOLDER'], video_File.filename)
-        video_File.save(file_Path)
+        # file_Path = os.path.join(app.config['UPLOAD_FOLDER'], video_File.filename)
+        video_File.save('uploads/'+video_File.filename)
+        # video_File.save(file_Path)
         
         # Process the video file using your ML model
         # prediction = process_video(file_path)
         
         # Return prediction as response
         # return jsonify({'prediction': prediction})
+        return 'File uploaded successfully', 200
     else:
         return jsonify({'error': 'Invalid file format'})
 
--- frontend/src/pages/Analyze.js
+++ frontend/src/pages/Analyze.js
@@ -1,31 +1,33 @@
 import React, { useState } from 'react'
+import axios from 'axios'
 
 export default function Analyze() {
-    const [isLoading, setisLoading] = useState(true)
+    const [isLoading, setisLoading] = useState(false)
     const [file, setFile] = useState(null)
     const [uploadStatus, setUploadStatus] = useState(null);
 
-    const handleFormSubmit = async(e) => {
+    const handleFormSubmit = async (e) => {
         e.preventDefault();
         if (!file) {
             alert('Please select a file to upload');
             return;
-          }
-      
-          const formData = new FormData();
-          formData.append('videoFile', file);
-      
-        //   try {
-        //     const response = await axios.post('/upload', formData, {
-        //       headers: {
-        //         'Content-Type': 'multipart/form-data'
-        //       }
-        //     });
-            
-        //     setUploadStatus(response.data);
-        //   } catch (error) {
-        //     console.error('Error uploading file:', error);
-        //   }
+        }
+
+        const formData = new FormData();
+        formData.append('videoFile', file);
+
+        try {
+            const response = await axios.post('http://localhost:5000/upload', formData, {
+                headers: {
+                    'Content-Type': 'multipart/form-data'
+                }
+            });
+
+            // setUploadStatus(response);
+            console.log(response);
+        } catch (error) {
+            console.error('Error uploading file:', error);
+        }
 
     }
     const handleFileChange = (e) => {
@@ -36,21 +38,21 @@ export default function Analyze() {
             <div className="container">
                 <h2 className='title'>Identify deepfake videos</h2>
                 <form onSubmit={handleFormSubmit} className='form'>
-                    <div class="input-container">
-                        <label for="images" class="drop-container" id="dropcontainer">
-                            <span class="drop-title">Drop video file here</span>
+                    <div className="input-container">
+                        <label for="images" className="drop-container" id="dropcontainer">
+                            <span className="drop-title">Drop video file here</span>
                             or
                             <input type="file" id="images" required onChange={handleFileChange} />
                         </label>
                     </div>
                     <button className='btn' type='submit' disabled={isLoading ? true : false}>Generate</button>
                 </form>
-                {isLoading ? <div class="loader">
-                    <div class="load-inner load-one"></div>
-                    <div class="load-inner load-two"></div>
-                    <div class="load-inner load-three"></div>
-                    <div class="load-inner load-four"></div>
-                    <span class="text">Analyzing...</span>
+                {isLoading ? <div className="loader">
+                    <div className="load-inner load-one"></div>
+                    <div className="load-inner load-two"></div>
+                    <div className="load-inner load-three"></div>
+                    <div className="load-inner load-four"></div>
+                    <span className="text">Analyzing...</span>
                 </div> : ''}
 
             </div>
